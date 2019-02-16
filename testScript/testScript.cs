b28b407c3f5bd2f9a629a65bff32b4d3cd0919c3
Tarmo Pikaro <tarmo.pikaro@visualcomponents.com>
2019-02-16 19:23:25 +0200

    Initial prototype for C# hot-reloadable C# script

---
+++ testScript/testScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    // Must be here when using EnvDTE
+    [STAThread]
+    public static void Main()
+    {
+        ScriptHost.ConnectDebugger();
+        Console.WriteLine("---------------------------------------------------------------------------");
+        // Must be able to share any data with application itself.
+        Class1.HelloClass1();
+        if(Class1.dataList.Count < 2)
+            Class1.dataList.Add("testScript::Main");
+
+        Console.WriteLine(DateTime.Now.ToString());
+        Console.WriteLine(__FILE__() + ": Hello ! world");
+    }
+
+    static string __FILE__([System.Runtime.CompilerServices.CallerFilePath] string fileName = "")
+    {
+        return fileName;
+    }
+}
